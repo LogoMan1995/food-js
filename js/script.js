7eaf8bb4e998c0329443cbd18e943f98c3a3ba1a
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-19 12:57:33 +0400

    rest

--- js/script.js
+++ js/script.js
@@ -162,12 +162,13 @@ function showModalByScroll (){
 
 
 class MenuCard{
-    constructor(src, alt, title, descr, price,parentSelector){
+    constructor(src, alt, title, descr, price,parentSelector,...classes){
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 27;
         this.changeToUAH();
@@ -177,10 +178,16 @@ class MenuCard{
         this.price = this.price * this.transfer;
     }
 
-    render(){
+    render() {
         const element = document.createElement('div');
+
+        if (this.classes.length === 0) {
+            this.classes = "menu__item";
+            element.classList.add(this.classes);
+        } else {
+            this.classes.forEach(className => element.classList.add(className));
+        }
         element.innerHTML = `
-        <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
@@ -189,7 +196,6 @@ class MenuCard{
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
                   `;
                   this.parent.append(element);
     }
